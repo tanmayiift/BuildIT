d45b6967d51567d6ff7f8f355b4b23d176702a1b
Tanmay Kumar <tanmay_d21@iift.edu>
2026-09-01 00:27:53 +0530

    fix: use BuildIT-owned runner image

--- convex/lib/runtimeVersion.ts
+++ convex/lib/runtimeVersion.ts
@@ -1,1 +1,1 @@
-export const RUNNER_IMAGE_VERSION = "vcr.vercel.com/pulsetrade/buildit-content-broker/buildit-runner@sha256:44b2bf68509f225fd81bd3f828f3aad2b4e5f448e58d35ed8dc42d43f17b7fbb";
+export const RUNNER_IMAGE_VERSION = "vcr.vercel.com/buildit-agentic-review/buildit-content-broker/buildit-runner@sha256:44b2bf68509f225fd81bd3f828f3aad2b4e5f448e58d35ed8dc42d43f17b7fbb";
